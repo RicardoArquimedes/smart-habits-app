717c96d5397a456e2406aa12fe98061fbff4505c
RicardoArquimedes <richardcqt28@gmail.com>
2026-01-02 08:59:38 -0500

    🧩 Add habit item presentational component

---
+++ src/app/features/habits/components/habit-item/habit-item.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Habit } from '../../../core/models/habit.model';
+
+@Component({
+  selector: 'app-habit-item',
+  standalone: true,
+  templateUrl: './habit-item.component.html',
+  styleUrl: './habit-item.component.scss',
+})
+export class HabitItemComponent {
+  // INPUT: dato que NO me pertenece
+  @Input({ required: true }) habit!: Habit;
+
+  // OUTPUT: aviso de algo que pasó
+  @Output() toggle = new EventEmitter<string>();
+
+  onToggle() {
+    this.toggle.emit(this.habit.id);
+  }
+}
